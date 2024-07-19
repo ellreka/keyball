adb9d137f4f5f020d3b65a3736c90276641d75b8
ellreka <ellreka5364@gmail.com>
2024-07-19 15:50:37 +0900

    feat: update keyball61 keymap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/ellreka/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/ellreka/keymap.c
@@ -38,15 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_QUOTE     , KC_COMMA     , KC_DOT     , KC_P     , KC_Y     ,                                  KC_Z     , KC_G     , KC_C     , KC_R     , KC_L     , KC_PLUS  ,
     KC_TAB    , KC_A   , KC_O     , KC_E     , KC_U     , KC_I     ,                                  KC_D     , KC_H     , KC_T     , KC_N     , KC_S  , LT(2, KC_MINUS)  ,
     LM(4, MOD_LSFT)    ,KC_SCLN     , KC_Q     , KC_J     , KC_K     , KC_X     , KC_LBRC  ,              KC_RBRC, KC_B     , KC_M     , KC_W  , KC_V   , KC_F  , LT(3, KC_SLSH)  ,
-    KC_LCTL  , KC_LALT  , KC_LANG2  , KC_ESC,KC_LGUI, KC_SPC, KC_ENT,                        KC_BSPC,KC_LANG1_ENT,_______,_______, _______ , KC_RALT  , KC_BSLS
+    KC_LCTL  , KC_LALT  , KC_LNG2  , KC_ESC,KC_LGUI, KC_SPC, KC_ENT,                        KC_BSPC,KC_LANG1_ENT,_______,_______, _______ , KC_RALT  , KC_BSLS
   ),
-  // Windows Layer
+  // QWERTY(Cmd, Alt, Ctrl) Layer
   [6] = LAYOUT_universal(
-    _______   , _______     , _______     , _______     , _______     , _______     ,                                  _______     , _______     , _______     , _______     , _______     , _______  ,
-    _______   , _______     , _______     , _______     , _______     , _______     ,                                  _______     , _______     , _______     , _______     , _______     , _______  ,
-    _______   , _______     , _______     , _______     , _______     , _______     ,                                  _______     , _______     , _______     , _______     , _______  , _______  ,
-    _______    , _______     , _______     , _______     , _______     , _______     , _______  ,              _______, _______     , _______     , _______  , _______   , _______  , _______  ,
-    KC_LGUI  , _______  , _______  , _______,KC_LCTL,_______,_______,                                          _______,_______,_______,_______, _______ , _______  , _______
+    _______   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , _______  ,
+    _______   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I    , KC_O     , KC_P     , _______  ,
+    _______   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , _______  ,
+    _______    , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , _______  ,              _______, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , _______  ,
+    _______  , _______  , _______  , _______,_______,_______,_______,    _______,_______,_______,_______, _______ , _______  , _______
   ),
   // Japanese Layer
   [5] = LAYOUT_universal(
@@ -83,11 +83,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Mouse Scroll Layer
   [3] = LAYOUT_universal(
-    _______  , DF(1)  , TG(6)      , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______ , _______ , _______  ,
+    _______  , DF(1)    , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______ , _______ , _______  ,
     _______  , _______  , _______  , _______  , G(KC_PMNS)  , G(KC_PPLS) ,                                  _______  , _______  , _______  , _______ , _______  , _______  ,
     _______ , KC_KB_VOLUME_DOWN,KC_KB_VOLUME_UP,KC_KB_MUTE, _______  , _______  ,                       _______  , _______  , KC_UP  , _______ , _______ , _______  ,
-    _______  , _______  , _______ , _______ , _______  , _______  , EEP_RST  ,            _______  , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT   , _______  , _______  ,
-    RESET    , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
+    _______  , _______  , _______ , _______ , _______  , _______  , KBC_RST  ,            _______  , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT   , _______  , _______  ,
+    _______  , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
   ),
 };
 // clang-format on
@@ -108,26 +108,6 @@ void oledkit_render_info_user(void) {
 }
 #endif
 
-// bool macos_check(void) {
-//     wait_ms(1600);
-//     bool mac_mode = true;
-//     tap_code(KC_NLCK);
-//     wait_ms(100);
-//     if (host_keyboard_leds() & (1 << USB_LED_NUM_LOCK))
-//     {
-//     mac_mode = false;
-//     }
-//     tap_code(KC_NLCK);
-//     wait_ms(100);
-//     if (host_keyboard_leds() & (1 << USB_LED_NUM_LOCK))
-//     {
-//     mac_mode = false;
-//     }
-//     return mac_mode;
-// }
-
-// uint16_t prev_keycode = 0x00;
-
 static bool is_cancel_enter = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -145,75 +125,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_LGUI:
             if(record->event.pressed) {
                 layer_on(1);
-                tap_code(KC_LANG2);
+                tap_code(KC_LNG2);
             } else {
                 layer_off(1);
-                tap_code(KC_LANG2);
+                tap_code(KC_LNG2);
             }
             return true;
-        // case KC_LCTL:
-        // if(macos_check) {
-        //     if(record->event.pressed) {
-        //         register_code(keycode);
-        //         layer_on(1);
-        //     } else {
-        //         unregister_code(keycode);
-        //         layer_off(1);
-        //     }
-        // } else {
-        //     if(record->event.pressed) {
-        //         register_code(KC_LGUI);
-        //     } else {
-        //         unregister_code(KC_LGUI);
-        //     }
-        // }
-        // return false;
-        // case KC_LGUI:
-        // if(macos_check) {
-        //     if(record->event.pressed) {
-        //         register_code(keycode);
-        //         layer_on(1);
-        //     } else {
-        //         unregister_code(keycode);
-        //         layer_off(1);
-        //     }
-        // } else {
-        //     if(record->event.pressed) {
-        //         register_code(KC_LCTL);
-        //     } else {
-        //         unregister_code(KC_LCTL);
-        //     }
-        // }
-        // return false;
         // TapしたときはEnterを、Holdしたときは日本語入力に切り替える
         case KC_LANG1_ENT:
             if(record->event.pressed) {
                 layer_on(5);
-                tap_code(KC_LANG1);
+                tap_code(KC_LNG1);
             } else {
-                layer_off(5);
+                tap_code(KC_LNG2);
                 if(!is_cancel_enter){
                     tap_code(KC_ENT);
                 }
-                tap_code(KC_LANG2);
+                layer_off(5);
                 is_cancel_enter = false;
             }
             return false;
         case KC_CANCEL_ENT:
             is_cancel_enter = true;
-            // if(record->event.pressed) {
-            //     is_cancel_enter = true;
-            // } else {
-            //     is_cancel_enter = false;
-            // }
             return false;
-        // case KC_ESC:
-        //     if(record->event.pressed) {
-        //         register_code(KC_LANG2);
-        //     } else {
-        //         unregister_code(KC_LANG2);
-        //     }
-        //     return true;
     }
     return true;
 }
